b3d25bdd3c077a9c65e0a0fc1cb0400eaaa1662d
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 23:36:27 +0500

    Task3

---
+++ Tyuiu.TishkinaEA.Sprint4.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.TishkinaEA.Sprint4.Task3.V13.Lib
+{
+    public class DataService : ISprint4Task3V13
+    {
+        public int Calculate(int[,] array)
+        {
+            if (array == null || array.GetLength(1) < 3)
+            {
+                throw new ArgumentException("Массив должен содержать минимум три столбца и не быть null.");
+            }
+
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                sum += array[i, 2]; // Индекс 2 соответствует третьему столбцу
+            }
+
+            return sum;
+        }
+    }
+}
