9046bbe90990d854ef8f017d2fa6a7d1eab71d77
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 23:33:59 +0500

    Task2

---
+++ Tyuiu.TishkinaEA.Sprint4.Task2.V11.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.TishkinaEA.Sprint4.Task2.V11.Lib
+{
+    public class DataService : ISprint4Task2V11
+    {
+        public int Calculate(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException("Массив не должен быть пустым или null.");
+            }
+
+            int sum = 0;
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint4.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint4.Task2.V11.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
