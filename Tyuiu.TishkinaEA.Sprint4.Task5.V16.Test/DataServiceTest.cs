694dfe3a2d309e7098e140a2be6404d79b0425b5
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 23:44:04 +0500

    Task5

---
+++ Tyuiu.TishkinaEA.Sprint4.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint4.Task5.V16.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint4.Task5.V16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TishkinaEA.Sprint4.Task5.V16.Lib;
+namespace Tyuiu.TishkinaEA.Sprint4.Task5.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Тишкина Елизавета Андреевна | РППБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Тишкина Елизавета Андреевна | РППБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
