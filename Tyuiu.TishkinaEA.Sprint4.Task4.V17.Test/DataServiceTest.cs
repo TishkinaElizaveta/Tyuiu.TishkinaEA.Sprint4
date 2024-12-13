7020bdcee1640fce5ff1e05d6be50cbf08eb6841
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-13 23:41:27 +0500

    Task4

---
+++ Tyuiu.TishkinaEA.Sprint4.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint4.Task4.V17.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
